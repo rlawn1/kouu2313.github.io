ddec572c4fa5e680ab88f9f46b7267a4a95b0ed8
rlawn1 <129610352+rlawn1@users.noreply.github.com>
2023-05-29 01:40:53 +0900

    Add files via upload

---
+++ js/weather.js
@@ -0,0 +1,19 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "1b4864071be0d8b4d3902eab22dc5cb0";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
